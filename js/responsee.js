134a338863868e15aafb2ce8b9dae216404dae10
keshavamurthy.ashwathanarayana <keshavamurthy.ashwathanarayana@cgi.com>
2020-12-22 22:30:09 +0530

    Contact Us form added

--- js/responsee.js
+++ js/responsee.js
@@ -169,4 +169,23 @@ jQuery(document).ready(function($) {
   $('.aside-nav a').filter(function() {
     return this.href == url;
   }).parent('li').parent('ul').parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item');
+
+
+  $('#sendMail').click(function(){
+    
+    Email.send({ 
+      Host: "smtp.gmail.com", 
+      Username: "sender@email_address.com", 
+      Password: "Enter your password", 
+      To: 'abhics40@gmail.com', 
+      From: "abhics40@gmail.com", 
+      Subject: "Sending Email using javascript", 
+      Body: "Well that was easy!!", 
+    }) 
+      .then(function (message) { 
+        alert("mail sent successfully") 
+      }); 
+
+
+  });
 });
